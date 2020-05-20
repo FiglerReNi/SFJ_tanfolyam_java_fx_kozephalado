1522efab5f8d2d92e591f0686243bbab0db1e2a8
FiglerReNi <renatafigler85@gmail.com>
2020-05-20 17:31:21 +0200

    forras

---
+++ PhoneBook/src/phonebook/ViewController.java
@@ -0,0 +1,22 @@
+package phonebook;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class ViewController implements Initializable {
+    
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+    
+}
